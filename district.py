22e0649e44c1ae9c057643c0e31d723d770b78cb
Kamil Cisek <01178216@pw.edu.pl>
2023-07-01 21:19:36 +0200

    Disctricts reading added

--- district.py
+++ district.py
@@ -1,13 +1,52 @@
 class District:
-    def __init__(self, name, id, n_seats, votes_previous: None):
+    def __init__(self, name, id, n_seats, previous_results: None,
+                 number_of_votes: None):
         self._name = name
         self._id = id
         self._n_seats = n_seats
+        self._sum_of_votes = 0
+        self._results = {}
 
-        if votes_previous is not None:
-            self._votes_previous = votes_previous
+        if number_of_votes is not None:
+            self._sum_of_votes = number_of_votes
 
-    def add_votes(self, party, votes_percent):
-        if self._votes_previous is None:
-            self._votes_previous = {}
-        self._votes_previous[party] = votes_percent
+        if previous_results is not None:
+            self._results = previous_results
+
+    def get_name(self):
+        return self._name
+
+    def get_id(self):
+        return self._id
+
+    def get_n_seats(self):
+        return self._n_seats
+
+    def get_sum_of_votes(self):
+        return self._sum_of_votes
+
+    def get_previous_results(self):
+        return self._results
+
+    def set_sum_of_votes(self, number_of_votes):
+        self._sum_of_votes = number_of_votes
+
+    def get_number_of_votes(self, party):
+        if self._results is None:
+            return 0
+        return self._results[party]
+
+    def add_votes(self, party, votes):
+        self._results[party] = votes
+
+    # Scale votes by given scale, scale is a dict {party: scale}
+    def scale_votes(self, scale_dict):
+        for party in self._results:
+            self._results[party] *= scale_dict[party]
+
+    # If votes for all parties do not sum up to sum of votes,
+    # which was set, scale them
+    def rescale_votes_to_100_percent(self):
+        if self._sum_of_votes > 0:
+            scale = self._sum_of_votes / sum(self._results.values())
+            self.scale_votes({party: scale for party in self._results})
--- district_database.py
+++ district_database.py
@@ -1,3 +1,9 @@
+import csv
+from district import District
+
+LIST_OF_PARTIES = ["PiS", "KO", "Lewica", "PSL", "Konfederacja", "Inne"]
+
+
 class DistrictDatabase:
     def __init__(self, districts_path: None, previous_election_path: None):
         self._districts = {}
@@ -8,13 +14,34 @@ class DistrictDatabase:
             self._was_loaded = True
 
     def load_database(self, districts_path, previous_election_path):
-        pass
+        with open(districts_path, 'r') as districts_file, \
+                open(previous_election_path, 'r') as previous_election_file:
+            self._read_database_from_file(
+                districts_file, previous_election_file)
 
     def _read_database_from_file(self, districts_file, previous_election_file):
-        pass
+        self._read_disctricts(districts_file)
+        self._read_previous_election(previous_election_file)
 
     def _read_disctricts(self, districts_file):
-        pass
+        reader = csv.DictReader(districts_file)
+        for row in reader:
+            self._districts[row["Numer okręgu"]] = District(
+                row["Siedziba OKW"],
+                row["Numer okręgu"],
+                row["Liczba mandatów"]
+            )
 
     def _read_previous_election(self, previous_election_file):
-        pass
+        reader = csv.DictReader(previous_election_file)
+        for row in reader:
+            district_id = row["Numer okręgu"]
+            # Set sum of votes
+            self._districts[district_id].set_sum_of_votes(
+                int(row["Liczba głosów ważnych"]))
+            # Add votes for each party
+            for party in LIST_OF_PARTIES:
+                self._districts[district_id].add_votes(
+                    party, int(row[party]))
+            # Scale votes to 100% if they do not sum up to sum of votes
+            self._districts[district_id].rescale_votes_to_100_percent()
